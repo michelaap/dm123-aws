9fa683b7a37a129087283f15f4693bb92f081489
unknown <michel.aap@hotmail.com>
2019-09-14 14:25:10 -0300

    add Dockerfile

--- aws_project01/src/main/java/br/com/siecola/aws_project01/controller/TestController.java
+++ aws_project01/src/main/java/br/com/siecola/aws_project01/controller/TestController.java
@@ -1,7 +1,9 @@
 package br.com.siecola.aws_project01.controller;
 
+import br.com.siecola.aws_project01.service.TestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class TestController {
     private static final Logger log = LoggerFactory.getLogger(TestController.class);
 
+    @Autowired
+    private TestService testService;
+
     @GetMapping("/dog/{name}")
     public ResponseEntity<?> dogTest(@PathVariable String name) {
-        log.info("Test controller - name: {}", name);
+        log.info("Michel controller - name: {}", name);
+        log.info("Is matilde? {}", testService.isUserMatilde(name));
+
         return ResponseEntity.ok("Name: " + name);
     }
 }
