3a4cc0e4535ad1f8c009eb42067422dcd925c874
unknown <michel.aap@hotmail.com>
2019-09-14 10:51:23 -0300

    add log

--- aws_project01/src/main/java/br/com/siecola/aws_project01/AwsProject01Application.java
+++ aws_project01/src/main/java/br/com/siecola/aws_project01/AwsProject01Application.java
@@ -3,11 +3,17 @@ package br.com.siecola.aws_project01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class AwsProject01Application {
+	private static final Logger log = LoggerFactory.getLogger(AwsProject01Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsProject01Application.class, args);
+
+		log.info("Starting Project01 example");
 	}
 
 }
