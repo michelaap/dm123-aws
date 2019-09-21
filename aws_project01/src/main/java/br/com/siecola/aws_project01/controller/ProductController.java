8cc55947130c51bb6d36807ef10ad86b9fbd3c60
Michel <michel.augusto@pg.inatel.br>
2019-09-21 17:11:27 -0300

    finish project

--- aws_project01/src/main/java/br/com/siecola/aws_project01/controller/ProductController.java
+++ aws_project01/src/main/java/br/com/siecola/aws_project01/controller/ProductController.java
@@ -1,7 +1,9 @@
 package br.com.siecola.aws_project01.controller;
 
+import br.com.siecola.aws_project01.enums.EventType;
 import br.com.siecola.aws_project01.model.Product;
 import br.com.siecola.aws_project01.repository.ProductRepository;
+import br.com.siecola.aws_project01.service.ProductPublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +16,14 @@ import java.util.Optional;
 @RequestMapping("/api/products")
 public class ProductController {
 
+    private ProductPublisher productPublisher;
     private ProductRepository productRepository;
 
     @Autowired
-    public ProductController(ProductRepository productRepository) {
+    public ProductController(ProductRepository productRepository,
+                             ProductPublisher productPublisher) {
         this.productRepository = productRepository;
+        this.productPublisher = productPublisher;
     }
 
     @GetMapping
@@ -49,6 +54,11 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<Product> saveProduct(
             @RequestBody @Valid Product product) {
+
+        Product productCreated = productRepository.save(product);
+        productPublisher.publishProductEvent(productCreated,
+                EventType.PRODUCT_CREATED, "matilde");
+
         return new ResponseEntity<Product>(productRepository.save(product),
                 HttpStatus.CREATED);
     }
@@ -58,6 +68,10 @@ public class ProductController {
             @RequestBody @Valid Product product, @PathVariable("id") long id) {
         if (productRepository.existsById(id)) {
             product.setId(id);
+
+            productPublisher.publishProductEvent(product,
+                    EventType.PRODUCT_UPDATE, "doralice");
+
             return new ResponseEntity<Product>(productRepository.save(product),
                     HttpStatus.OK);
         } else {
@@ -70,6 +84,10 @@ public class ProductController {
         Optional<Product> optProduct = productRepository.findById(id);
         if (optProduct.isPresent()) {
             Product product = optProduct.get();
+
+            productPublisher.publishProductEvent(product,
+                    EventType.PRODUCT_DELETED, "hannah");
+
             productRepository.delete(product);
             return new ResponseEntity<Product>(product, HttpStatus.OK);
         } else {
