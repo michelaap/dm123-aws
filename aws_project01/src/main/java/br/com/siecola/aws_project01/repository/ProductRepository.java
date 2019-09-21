75bc6b9d91a3addc18307471685728b57d864fad
Michel <michel.augusto@pg.inatel.br>
2019-09-21 11:13:50 -0300

    add product controller

--- aws_project01/src/main/java/br/com/siecola/aws_project01/repository/ProductRepository.java
+++ aws_project01/src/main/java/br/com/siecola/aws_project01/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package br.com.siecola.aws_project01.repository;
 import br.com.siecola.aws_project01.model.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    Optional<Product> findByCode(String code);
 }
